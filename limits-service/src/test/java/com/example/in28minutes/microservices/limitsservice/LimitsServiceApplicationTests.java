f55964876285a84bed5e660b961cb3bc1283959b
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-06-17 23:10:38 +0300

    first commit

---
+++ limits-service/src/test/java/com/example/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.in28minutes.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
